022d5cb69209a4609a9acdffc5dec32e0c905581
Sergey Volchkov <debfantasys@gmail.com>
2021-11-27 19:02:56 +0700

    update card footer

--- src/components/Modals/TaxDeductionModal/TaxDeductionModal.tsx
+++ src/components/Modals/TaxDeductionModal/TaxDeductionModal.tsx
@@ -8,19 +8,33 @@ const TaxDeductionModal: React.FC = () => {
     return (
         <div className="tax-modal">
             <div className="card">
+                <CloseIcon/>
+                <h4 className="card-title">
+                    Налоговый вычет
+                </h4>
+                <p className="card-description">
+                    Используйте налоговый вычет чтобы погасить ипотеку досрочно. Размер налогового вычета составляет не
+                    более 13% от своего официального годового дохода.
+                </p>
+
                 <div className="card-container">
-                    <CloseIcon/>
-                    <h4 className="card-title">
-                        Налоговый вычет
-                    </h4>
-                    <p className="card-description">
-                        Используйте налоговый вычет чтобы погасить ипотеку досрочно. Размер налогового вычета составляет не
-                        более 13% от своего официального годового дохода.
-                    </p>
-
                     <CalculateTaxInput/>
-
                     <EarlyPayments/>
+                </div>
+
+                <div className="card-footer">
+
+                    <div className="reduce">
+                        <p>Что уменьшаем?</p>
+                        <div className="reduce-container">
+                            <div className="budge active">
+                                Платеж
+                            </div>
+                            <div className="budge">
+                                Срок
+                            </div>
+                        </div>
+                    </div>
 
                     <button className="btn btn-primary">
                         Добавить
